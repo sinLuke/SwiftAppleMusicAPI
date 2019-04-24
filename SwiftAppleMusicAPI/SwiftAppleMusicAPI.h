7ff6b2ae1b96e405e09ef40cac4aacfdaf2b26cd
sinLuke <yinluke@me.com>
2019-04-24 14:25:15 -0400

    push

---
+++ SwiftAppleMusicAPI/SwiftAppleMusicAPI.h
@@ -0,0 +1,19 @@
+//
+//  SwiftAppleMusicAPI.h
+//  SwiftAppleMusicAPI
+//
+//  Created by Luke on 2019-04-24.
+//  Copyright © 2019 luke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftAppleMusicAPI.
+FOUNDATION_EXPORT double SwiftAppleMusicAPIVersionNumber;
+
+//! Project version string for SwiftAppleMusicAPI.
+FOUNDATION_EXPORT const unsigned char SwiftAppleMusicAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftAppleMusicAPI/PublicHeader.h>
+
+
